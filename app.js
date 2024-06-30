4cc93e7e66d06e9c4e26ae312c826bc832a6069d
mikaelnovaes <159655604+mikaelnovaes@users.noreply.github.com>
2024-06-30 14:54:21 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+//1.Mostre um alerta com a mensagem "Boas vindas ao nosso site!".
+alert("boas vindas ao meu site!");
+
+//2.	Declare uma variável chamada nome e atribua a ela o valor "Lua".
+let nome = 'lua';
+
+//3.	Crie uma variável chamada idade e atribua a ela o valor 25.
+let idade = 25;
+
+//4.	Defina uma variável numeroDeVendas e atribua a ela o valor 50.
+let numeroDeVendas = 50;
+
+//5.	Defina uma variável saldoDisponivel e atribua a ela o valor 1000.
+let saldoDisponivel = 1000;
+
+//6.	Exiba um alerta com o texto "Erro! Preencha todos os campos"
+alert("Erro! Preencha todos os campos");
+
+//7.	Declare uma variável chamada mensagemDeErro e atribua a ela o valor "Erro! Preencha todos os campos" Agora exiba um alerta com o valor da variável mensagemDeErro.
+let mensagemDeErro = "Erro! Preencha todos os campos2"
+alert(mensagemDeErro);
+
+//8.	Use um prompt para perguntar o nome do usuário e armazená-lo na variável nome.
+
+let nomeUsuário = prompt("Qual nome do usuário?");
+
+//9.	Peça ao usuário para digitar sua idade usando um prompt e armazene-a na variável idade.
+let idade2 = prompt('Digite a sua idade');
+
+//10.	Agora, caso a idade seja maior ou igual que 18, exiba um alerta com a mensagem "Pode tirar a habilitação!".
+let idade3 = prompt('Digite a sua idade');
+if (idade3 >= 18) {
+    alert('Pode tirar a habilitação!'); 
+}else {
+
+    alert('Não pode tirar a habilitação!'); 
+}
